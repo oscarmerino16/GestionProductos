ca3dd31a2865291133df504dddca7db8400f3670
oscar <omc16@outlook.com>
2021-01-20 11:32:29 +0100

    inicio

---
+++ src/EjercicioModulos/Principal.java
@@ -0,0 +1,22 @@
+package EjercicioModulos;
+
+/**
+ * 
+ * @author oscar merino
+ * @version 1.0
+ */
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Modulo m = new Modulo("Programación", "PRG", 300);
+		
+		System.out.println(m.toString());
+		System.out.println("Numero de horas del ciclo: " + m.getNumHoras());
+
+	
+
+	}
+
+}
