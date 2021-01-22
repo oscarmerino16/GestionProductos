643451ba306758a799f1b01f9bfe236d9b1308ff
oscar <omc16@outlook.com>
2021-01-22 13:10:00 +0100

    version3.0 finalizada

--- src/EjercicioModulos/Curso.java
+++ src/EjercicioModulos/Curso.java
@@ -7,13 +7,39 @@ public class Curso {
 
 	private String nombre;
 	private String tutor;
-	private Modulos vModulos[];
+	private static Modulos vModulos[];
 	
 	public Curso(String nombre, String tutor) {
 		
 		this.nombre = nombre;
 		this.tutor = tutor;
 		this.vModulos= new Modulos[10];
+	}
+	
+
+	public static void nuevoModulo() {
+		Scanner leer = new Scanner(System.in);
+		String nueModulo = "";
+		String nueCodigo = "";
+		int nueHoras = 0;
+		int totalHoras = 0;
+
+		System.out.println("Dime el nombre del nuevo modulo:");
+		nueModulo = leer.next();
+		System.out.println("Dime el codigo del nuevo modulo:");
+		nueCodigo = leer.next();
+		System.out.println("Dime cuantas horas tiene el modulo:");
+		nueHoras = leer.nextInt();
+		for (int i = 0; i < vModulos.length; i++) {
+			if (vModulos[i] == null) {
+				vModulos[i] = new Modulos(nueModulo, nueCodigo, nueHoras);
+				totalHoras = totalHoras + nueHoras;
+				break;
+			}
+		}
+		for (int j = 0; j < 10; j++) {
+			System.out.println("\n");
+		}
 	}
 
 	public String getNombre() {
--- src/EjercicioModulos/Principal.java
+++ src/EjercicioModulos/Principal.java
@@ -36,19 +36,38 @@ import java.util.Scanner;
 			return num;
 	}	
 	
-	/*public static void añadirModulo() {
-		
-		for (int i = 0; i <i .length; i++) {
-			
-		}
-		
-		
-	}*/
-
+	
 	public static void main(String[] args) {
 		
-	
-		pintaMenu();
+		Curso c = new Curso("1º DAW", "José Micolau");
+		int cond = 0;
+		
+		do {
+
+			switch (pintaMenu()) {
+			case 1:Curso.nuevoModulo();
+
+				break;
+			case 2:
+				Scanner ler = new Scanner(System.in);
+				System.out.println(c.toString());
+				System.out.println("Pulse cualquier tecla para salir de ver todos los modulos");
+				String a; 
+				a = ler.next();
+				break;
+			case 3:
+				
+
+				break;
+			case 4:
+				cond = 1;
+
+				break;
+
+			default:
+				break;
+			}
+		} while (cond != 1);
 		
 		
 		
